529a414848358ea1689de91972e52a3f9e444509
VoMinhTan1 <68368933+VoMinhTan1@users.noreply.github.com>
2021-07-02 15:38:15 +0700

    upcode

---
+++ khuyenmai.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Khuyến Mãi</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+	<link rel="stylesheet" href="css/khuyenmai.css">
+	<link rel="stylesheet" href="css/reponsive-menu.css">
+</head>
+<body>
+<div class="main" style='overflow: hidden;'>
+			<div class="khuyenmai" style='background: url(img/bg_pink.jpg);'>
+				<div class="container">
+					<div class="row">
+						<div class="col-xs-6">
+							<img src="img/km1.jpg" alt="" width='100%'>
+							<p class="td">Từ Ngày 9/6-11/6/2016</p>
+							<p class="fg">Mua hàng vi diệu - Nhận ngay Voucher 200.000đ - 2.000.000đ miễn phí</p>
+						</div>
+						<div class="col-xs-6">
+							<img src="img/km2.jpg" alt="" width='100%'>
+							<p class="td">Từ Ngày 1/6-30/6/2016</p>
+							<p class="fg">Tháng Samsung quà thật sung.Nhận ngay Voucher 200.000đ khi mua hàng</p>
+						</div>
+						<div class="col-xs-6 col-sm-4 kkl">
+							<img src="img/km3.jpg" alt="" width='100%'>
+							<p class="td">Từ ngày 01/06 - 30/06/2016</p>
+							<p class="fg">Đón EURO 2016 Ưu đãi lớn cùng iPhone 6s</p>
+						</div>
+						<div class="col-xs-6 col-sm-4 kkl">
+							<img src="img/km4.jpg" alt="" width='100%'>
+							<p class="td">Từ ngày 01/06 - 30/06/2016</p>
+							<p class="fg">EURO thật sốt rước điện thoại giá tốt</p>
+						</div>
+						<div class="col-xs-6 col-sm-4 kkl">
+							<img src="img/km5.jpg" alt="" width='100%'>
+							<p class="td">Từ ngày 01/06 - 30/06/2016</p>
+							<p class="fg">HUAWEI GR5 quay cuồng EURO giá mới cực sốc</p>
+						</div>
+						<div class="col-xs-6 col-sm-4 kkl">
+							<img src="img/km6.jpg" alt="" width='100%'>
+							<p class="td">Từ ngày 01/06 - 30/06/2016</p>
+							<p class="fg">Đặt trước Sony Xperia X | XA</p>
+						</div>
+						<div class="col-xs-6 col-sm-4 kkl">
+							<img src="img/km7.jpg" alt="" width='100%'>
+							<p class="td">Từ ngày 01/06 - 30/06/2016</p>
+							<p class="fg">Sút bay nỗi lo về giá</p>
+						</div>
+						<div class="col-xs-6 col-sm-4 kkl">
+							<img src="img/km8.jpg" alt="" width='100%'>
+							<p class="td">Từ ngày 01/06 - 30/06/2016</p>
+							<p class="fg">Đặt trước Tablet Lenovo Tab 3 Essential</p>
+						</div>
+						<div class="col-xs-6 col-sm-4 kkl">
+							<img src="img/km9.jpg" alt="" width='100%'>
+							<p class="td">Từ ngày 01/06 - 30/06/2016</p>
+							<p class="fg">Tưng bừng khai trương chi nhánh thứ 249 tại Lạng Sơn</p>
+						</div>
+						<div class="col-xs-6 col-sm-4 kkl">
+							<img src="img/km10.jpg" alt="" width='100%'>
+							<p class="td">Từ ngày 01/06 - 30/06/2016</p>
+							<p class="fg">Miễn phí 100% chi phí sửa chữa và chăm sóc điện thoại tại Viễn Thông A</p>
+						</div>
+						<div class="col-xs-6 col-sm-4 kkl">
+							<img src="img/km11.jpg" alt="" width='100%'>
+							<p class="td">Từ ngày 01/06 - 30/06/2016</p>
+							<p class="fg">Đặt trước Samsung Galaxy Tab A 10.1</p>
+						</div>
+						<div class="col-xs-6 col-sm-4 kkl">
+							<img src="img/km12.jpg" alt="" width='100%'>
+							<p class="td">Từ ngày 01/06 - 30/06/2016</p>
+							<p class="fg">Mua iXpand tại Viễn Thông A - Xem phim bom tấn thả ga</p>
+						</div>
+						<div class="col-xs-6 col-sm-4 kkl">
+							<img src="img/km13.jpg" alt="" width='100%'>
+							<p class="td">Từ ngày 01/06 - 30/06/2016</p>
+							<p class="fg">Đón chào EURO – Mua ngay OPPO - Nhận quà cực chất</p>
+						</div>
+						<div class="col-xs-6 col-sm-4 kkl">
+							<img src="img/km14.jpg" alt="" width='100%'>
+							<p class="td">Từ ngày 01/06 - 30/06/2016</p>
+							<p class="fg">Tưng bừng khai trương chi nhánh thứ 248 tại Quy Nhơn - Bình Định</p>
+						</div>
+					</div>
+				</div>
+			</div>
+</div>
+</body>
+</html>
