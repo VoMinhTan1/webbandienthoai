ebbe821f049c361804f6a3be0e0d9d8bf726d3aa
VoMinhTan1 <68368933+VoMinhTan1@users.noreply.github.com>
2021-07-02 19:13:43 +0700

    Update connectmysql.php

--- MYSQL/connectmysql.php
+++ MYSQL/connectmysql.php
@@ -1,14 +1,14 @@
-<?php 
-//khai bao thong tin ket noi
-	$servername = "localhost";
-	$username='root';
-	$password = "";
-	$dbname = "ishop";
-	// Create connection
-	//ket noi den co so du lieu
-	$connection = mysqli_connect($servername, $username, $password,$dbname) or die("Không Thể Kết Nối Tới Cơ Sở Dữ Liệu");
-	mysqli_set_charset($connection,"utf8");
-	// Change character set to utf8
-	//luon hien thi cho unicode
 
+<?php
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["us-cdbr-east-04.cleardb.com"];
+$cleardb_username = $cleardb_url["bbaff072d5d633"];
+$cleardb_password = $cleardb_url["c732ff80"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 ?>
+
